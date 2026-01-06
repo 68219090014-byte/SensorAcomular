31830f9d5501a6520db0026ce5d4926ce06f5ebf
68219090014-byte <68219090014@technictrang.ac.th>
2026-01-06 14:54:54 +0700

    Create Lab41.ino

---
+++ LAB4 KeyPAD 4x4/Lab41.ino
@@ -0,0 +1,35 @@
+/* LAB4.1 4x4 */
+// https://app.cirkitdesigner.com/project/e1b6e7ea-e255-4d98-bf3d-26d3d545b744
+
+#include <Keypad.h>
+
+const byte ROWS = 4; // Number of rows
+const byte COLS = 4; // Number of columns
+
+
+byte rowPins[ROWS] = {12, 11, 10, 9};   // Connect to R1, R2, R3, R4
+byte colPins[COLS] = {7, 6, 5, 4};   // Connect to C1, C2, C3, C4
+
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600); // Initialize serial communication
+  Serial.println("4x4 Keypad Test");
+}
+
+void loop() {
+  char key = keypad.getKey(); // Get the key pressed
+
+  if (key) { // If a key is pressed
+    Serial.print("Key Pressed: ");
+    Serial.println(key); // Print the key to the serial monitor
+  }
+}
